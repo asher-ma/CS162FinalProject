8c7fa140a1878037469e3ad03e88b862a6300e87
asher-ma <mangelas@oregonstate.edu>
2024-11-19 14:29:25 -0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import processing.core.PApplet;
+
+/**
+ * This program draws fish swimming around a fish tank
+ * 
+ * 
+ * @author Asher Mangel
+ * @version 1.0
+ * @since 1.0
+ */
+public class Main {
+    public static void main(String[] args) {
+        PApplet mySketch = new MySketch();
+	    String[] processingArgs = {"Asher's Sketch"};
+	    PApplet.runSketch(processingArgs, mySketch);
+    }
+}
