9ed5701c5d278890856645856fd116a425c6481b
asher-ma <mangelas@oregonstate.edu>
2024-11-21 12:31:43 -0800

    Tank class loading and displaying background image

--- src/Tank.java
+++ src/Tank.java
@@ -1,14 +1,17 @@
 import processing.core.PApplet;
+import processing.core.PImage;
 import java.util.ArrayList;
 
 public class Tank {
     PApplet p;
     int leftX, rightX, topY, botY;
+    PImage background;
 
     ArrayList<Fish> fish = new ArrayList<Fish>();
 
-    public Tank(PApplet p, int leftX, int rightX, int topY, int botY){
+    public Tank(PApplet p, String backgroundFile, int leftX, int rightX, int topY, int botY){
         this.p = p;
+        background = p.loadImage(backgroundFile);
         this.leftX = leftX;
         this.rightX = rightX;
         this.topY = topY;
@@ -16,6 +19,7 @@ public class Tank {
     }
 
     public void draw(){
+        p.image(background, leftX, topY, rightX-leftX, botY-topY);
         for (Fish fish: fish){
             fish.draw();
         }
