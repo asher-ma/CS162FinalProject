ebdbc37ffb2304bb1d54ea81cac614e6e432bff1
asher-ma <asherhmangel@gmail.com>
2024-12-09 14:50:58 -0800

    Fish classified as predators/prey

--- src/FishSketch.java
+++ src/FishSketch.java
@@ -9,10 +9,10 @@ public class FishSketch extends PApplet {
  
     public void setup(){
         tank = new Tank(this, this.loadImage("../data/tank.png"), 0, 570, 75, 390);
-        tank.newFish(this.loadImage("../data/orangeFish.png"), "Orange Fish");
-        tank.newFish(this.loadImage("../data/blueFish.png"), "Blue Fish");
-        tank.newFish(this.loadImage("../data/greenFish.png"), "Green Fish");
-        tank.newFish(this.loadImage("../data/meanFish.png"), "Green Fish");
+        tank.newFish(this.loadImage("../data/orangeFish.png"), "Orange Fish", false);
+        tank.newFish(this.loadImage("../data/blueFish.png"), "Blue Fish", false);
+        tank.newFish(this.loadImage("../data/greenFish.png"), "Green Fish", false);
+        tank.newFish(this.loadImage("../data/meanFish.png"), "Green Fish", true);
     }
 
     public void draw(){
--- src/Tank.java
+++ src/Tank.java
@@ -49,14 +49,14 @@ public class Tank {
         for (Fish fish: fish){
             Button button = fish.getButton();
             if (button.isInButton()){
-                this.fish.add(new Fish(p, fish.getImg(), 0, fish.getName(), leftX, rightX, topY, botY));
+                this.fish.add(new Fish(p, fish.getImg(), 0, fish.getName(), fish.isPredator(), leftX, rightX, topY, botY));
                 return;
             }
         }
     }
 
-    public void newFish(PImage fishImg, String name){
-        fish.add(new Fish(p, fishImg, fish.size()+1, name, leftX, rightX, topY, botY));
+    public void newFish(PImage fishImg, String name, boolean predator){
+        fish.add(new Fish(p, fishImg, fish.size()+1, name, predator, leftX, rightX, topY, botY));
     }
 
     // Function to bounce fish off of eachother
