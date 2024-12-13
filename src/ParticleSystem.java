b8719cdf9d33a4b09060bb2a97f39b6a4e7b086f
asher-ma <asherhmangel@gmail.com>
2024-12-12 17:12:05 -0800

    Added ParticleSystem vars and constructor

--- src/ParticleSystem.java
+++ src/ParticleSystem.java
@@ -1,3 +1,13 @@
+import processing.core.PApplet;
+
 public class ParticleSystem {
-    
+    private PApplet p;
+    private int count;
+    private DeathParticle[] particles;
+
+    ParticleSystem(PApplet p, float left, float right, float top, float bottom){
+        this.p = p;
+        count = (int)((right - left)/5);
+        particles = new DeathParticle[count];
+    }
 }
