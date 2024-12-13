d15252c76da25d9432dfea50486c7d0a48f9850a
asher-ma <asherhmangel@gmail.com>
2024-12-12 17:03:28 -0800

    Added particle variables and constructor

--- src/DeathParticle.java
+++ src/DeathParticle.java
@@ -1,3 +1,31 @@
+import processing.core.PApplet;
+
 public class DeathParticle {
+    private final float GRAVITY = (float)0.01;
+    private final int G = 0;
+    private final int B = 0;
+    private final int A = 150;
     
+    private PApplet p;
+    private float r;
+    private float time, life;
+    private float x, y;
+    private float ySpeed, xSpeed;
+    private float size;
+    
+    public DeathParticle(PApplet p, float x, float y){
+        this.p = p;
+        this.x = x;
+        this.y = y;
+
+        ySpeed = p.random(-1, 1);
+        ySpeed = p.random(-1, 1);
+
+        time = 0;
+        life = p.random(300);
+
+        r = p.random(70, 170);
+
+        size = p.random(3, 6);
+    }
 }
